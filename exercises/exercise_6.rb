c684150004b5dde79e05dc0ae5d56be3f28256c7
vagrant <lovemore_jokonya@yahoo.com>
2017-12-05 22:11:31 +0000

    one to many associations for creating employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find 1
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Ahurram", last_name: "Mirani", hourly_rate: 60)
+
+@store2 = Store.find 2
+@store2.employees.create(first_name: "LoveJ", last_name: "Jokes", hourly_rate: 60)
+@store2.employees.create(first_name: "Julius", last_name: "Hsu", hourly_rate: 60)
