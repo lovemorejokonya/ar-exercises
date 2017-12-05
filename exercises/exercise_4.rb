02cf5aa418b5dafc9af2b187f048efa77fcb1aaf
vagrant <lovemore_jokonya@yahoo.com>
2017-12-05 21:03:40 +0000

    loading subsets of stores exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,54 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+
+[
+  {
+    name: "Burnaby",
+    annual_revenue: 300000,
+    mens_apparel: true,
+    womens_apparel: true
+  },
+  {
+    name: "Richmond",
+    annual_revenue: 1260000,
+    mens_apparel: false,
+    womens_apparel: true
+  },
+  {
+    name: "Gastown",
+    annual_revenue: 190000,
+    mens_apparel: true,
+    womens_apparel: false
+  },
+  {
+    name: "Surrey",
+    annual_revenue: 224000,
+    mens_apparel: false,
+    womens_apparel: true
+  },
+  {
+    name: "Whistler",
+    annual_revenue: 1900000,
+    mens_apparel: true,
+    womens_apparel: false
+  },
+  {
+    name: "Yaletown",
+    annual_revenue: 430000,
+    mens_apparel: true,
+    womens_apparel: true
+  }
+].each do |store_attributes|
+  Store.create!(store_attributes)
+end
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts "#{store.name}, #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+@womens_stores.each do |wstore|
+  puts "#{wstore.name}, #{wstore.annual_revenue}"
+end
