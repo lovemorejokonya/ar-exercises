08750e33ff727ba83e78e718332aac1da989cb44
vagrant <lovemore_jokonya@yahoo.com>
2017-12-05 21:46:26 +0000

    sum average and revenue exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@revenue_sum = Store.sum :annual_revenue
+@revenue_average = Store.average :annual_revenue
+@revenue_over_million = Store.where("annual_revenue >= 1000000").size
+puts @revenue_sum
+puts @revenue_average
+puts @revenue_over_million
+
